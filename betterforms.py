0afa4fa80eb557c86fcbefaa1a42f15b3962714c
Silas Wright <siwright007@icloud.com>
2025-04-21 17:40:44 -0400

    user input done and looking nice :)

--- betterforms.py
+++ betterforms.py
@@ -4,8 +4,8 @@ import json
 # Read input from Node.js
 raw_input = sys.stdin.read()
 data = json.loads(raw_input)
-
 user_input = data.get('input', '')
-response = f'You typed: {user_input}'
+
+response = " "#this is where magic happens :)
 
 print(json.dumps(response))
